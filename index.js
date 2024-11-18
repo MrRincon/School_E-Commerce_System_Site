ba3ea64473aaa6729a7a3c09ea8a5ecc5a63a569
MrRincon <alamrdrgz98@gmail.com>
2024-11-18 19:54:39 +0000

    Removing currently unnecessary items in the vue object

--- index.js
+++ index.js
@@ -12,26 +12,7 @@ let webstore = new Vue({
             options: ['Subject', 'Location', 'Price', 'Spaces']
         },
         cart: [],//Array to store items in the shopping cart [1001,1001,1001]
-        order: {
-            firstName: '',
-            lastName: '',
-            phoneNumber: '',
-            address: '',
-            city: '',
-            zip: '',
-            states: {
-                state: '',
-                options: ['London', 'Manchester', 'Bristol', 'Cardiff']
-            },
-            method: 'Home',
-            gift: {
-                value: 'Do not send as gift',
-                options: {
-                    isGift: 'Send as gift',
-                    isNotGift: 'Do not send as gift'
-                }
-            }
-        }
+        
     },
     methods: {
         displayPage(page) {
@@ -90,47 +71,7 @@ let webstore = new Vue({
                 .slice(0, 3);
             return trendingLessons;
         },
-        itemsInCart() {
-            let currItems = [];
-            let existentItems = [];
-            this.cart.forEach(item => {
-                if (!existentItems.includes(item)) {
-                    let itemData = {
-                        "img": {
-                            "location": "",
-                            "alt": ""
-                        },
-                        "price": 0.00,
-                        "subject": "",
-                        "location": "",
-                        "id": 0,
-                        "amount": 0
-                    };
-                    this.lessons.forEach(lesson => {
-                        if (item === lesson.id) {
-                            itemData.img.location = lesson.img.location;
-                            itemData.img.alt = lesson.img.alt;
-                            itemData.subject = lesson.subject;
-                            itemData.price = lesson.price;
-                            itemData.subject = lesson.subject;
-                            itemData.location = lesson.location;
-                            itemData.id = lesson.id;
-                            itemData.amount++;
-                        }
-                    });
-                    currItems.push(itemData);
-                    existentItems.push(item);
-                } else {
-                    currItems.forEach(itemIn => {
-                        if(item === itemIn.id){
-                            itemIn.amount++;
-                        }
-                    })
-                }
-            });
-
-            return currItems;
-        }
+        
     },
     created: function () {
         fetch("http://localhost:3000/collections/lessons").then(
