e75aaf19c4ccc8542e5d6428dd5136ee20787219
MrRincon <alamrdrgz98@gmail.com>
2024-11-23 13:06:22 +0000

    Displaying the elements found in the search bafr and adding the search quey function

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ let webstore = new Vue({
                 console.log(`Error fetching post method: ${err}`);
             }
         },
-        reloadPage() {
+        reloadPage() {// Method to update the lessons when the page is reloaded after placing an order
             webstore.lessons = [];
             fetch("https://school-e-commerce-system-server.onrender.com/lessons").then(
                 function (res) {
@@ -138,7 +138,36 @@ let webstore = new Vue({
                     )
                 }
             );
-            this.displayPage(0);
+            this.displayPage(0);// Calling the function to display the main page agaim
+        },
+        async searchBarQuery(newQuery){// Method to search for lessons that match the search bar query
+            const modalContent = document.getElementById('searchModal-body');// Element targeted for the response
+            try {// Try-Catch for any errors with the server 
+                if(!newQuery.trim){// Check up for the input field to see if is empty
+                    modalContent.innerHTML = `<p><strong>No results, please type to search.</strong></p>`;
+                    return;
+                }
+                // Fetch the post method to the server to find the elements that match the query sent in the body as a request
+                const response = await fetch("https://school-e-commerce-system-server.onrender.com/search", {
+                    method: 'POST',
+                    headers: { 'Content-type' : 'application/json'},
+                    body:  JSON.stringify({ searchTerm: newQuery })
+                })
+                const results = await response.json();
+                if (results.length > 0) {// Display all the lessons found in the targeted element that match the query
+                    newString = ``;
+                    console.log(results);
+                    for ( let result of results){// Loop through the results to display each lessons information
+                        newString += `<p><strong>${result.subject} - ${result.location} - ${result.price}</strong></p><hr class='divider'>`;
+                    }
+                    modalContent.innerHTML = newString;
+                } else{// Error message if nothing is found
+                    modalContent.innerHTML = `<p><strong>No results found for ${newQuery}.</strong></p>`;
+                }
+            } catch (error) {
+                console.error('Error fetching the search results', error);
+                modalContent.innerHTML = `<p><strong>An error occurred. Please try later</strong></p>`;
+            }
         }
     },
     computed: {// Computed methods for the application
